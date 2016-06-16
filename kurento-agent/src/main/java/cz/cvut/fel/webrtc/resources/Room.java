aef4de445913a667f98601a3b664bd3c5af8f707
Pierre <pierre.coquerel@mines-ales.org>
2016-06-16 14:11:44 +0200

    Modif classe Participant et Room

--- kurento-agent/src/main/java/cz/cvut/fel/webrtc/resources/Room.java
+++ kurento-agent/src/main/java/cz/cvut/fel/webrtc/resources/Room.java
@@ -1,359 +1,324 @@
 /*
  * (C) Copyright 2014 Kurento (http://kurento.org/)
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package cz.cvut.fel.webrtc.resources;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import org.kurento.client.*;
+import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.kurento.client.Continuation;
+import org.kurento.client.ErrorEvent;
+import org.kurento.client.EventListener;
+import org.kurento.client.IceCandidate;
+import org.kurento.client.KurentoClient;
+import org.kurento.client.MediaPipeline;
+import org.kurento.room.api.RoomHandler;
+import org.kurento.room.exception.RoomException;
+import org.kurento.room.exception.RoomException.Code;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.socket.WebSocketSession;
-
-import javax.annotation.PreDestroy;
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ConcurrentSkipListMap;
-
-//import org.kurento.client.HubPort;
 
 /**
  * @author Ivan Gracia (izanmail@gmail.com)
- * @since 4.3.1
+ * @author Micael Gallego (micael.gallego@gmail.com)
+ * @author Radu Tom Vlad (rvlad@naevatec.com)
+ * @since 1.0.0
  */
-public class Room implements Closeable {
+public class Room {
+  public static final int ASYNC_LATCH_TIMEOUT = 30;
 
-	private final Logger log = LoggerFactory.getLogger(Room.class);
+  private final static Logger log = LoggerFactory.getLogger(Room.class);
 
-	private final ConcurrentMap<String, Participant> participants = new ConcurrentSkipListMap<>();
-	private MediaPipeline presentationPipeline;
-	private MediaPipeline compositePipeline;
-	private Composite composite;
-	private final String name;
-	
-	private Line line;
-	private final String callId;
-	private long cseq;
-	private boolean closing;
-	
-	private WebUser screensharer;
-	
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	protected Room(String roomName) {
-		this.callId = UUID.randomUUID().toString();
-		this.cseq = (new Random()).nextInt(100);
-		this.name = roomName;
-	}
+  private final ConcurrentMap<String, Participant> participants = new ConcurrentHashMap<String, Participant>();
+  private final String name;
 
-	public Room(String roomName, KurentoClient kurento) {
-		this(roomName);
-		
-		this.compositePipeline = kurento.createMediaPipeline();
-		this.presentationPipeline = kurento.createMediaPipeline();
-		this.composite = new Composite.Builder(compositePipeline).build();
-		
-		log.info("ROOM {} has been created", roomName);
-	}
+  private MediaPipeline pipeline;
+  private CountDownLatch pipelineLatch = new CountDownLatch(1);
 
-	@PreDestroy
-	private void shutdown() {
-		this.close();
-	}
+  private KurentoClient kurentoClient;
 
-	public Participant join(String userId, WebSocketSession session, Class<? extends Participant> sessionClass) {
-		log.info("ROOM {}: adding participant {}", name, userId);
-		
-		Participant participant = null;
-		
-		try {
-			
-			participant = sessionClass.getConstructor(
-					String.class,
-					String.class,
-					WebSocketSession.class,
-					MediaPipeline.class,
-					MediaPipeline.class,
-					Hub.class)
-			.newInstance(
-					userId,
-					this.name,
-					session,
-					this.compositePipeline,
-					this.presentationPipeline,
-					this.composite
-			);
-			
-			add(participant);
-			sendInformation(participant, "compositeInfo");
-			
-		} catch (Exception e) {
-			log.info("ROOM {}: adding participant {} failed: {}", name, userId, e);
-		}
-		
-		return participant;
-	}
-	
-	public void add(Participant participant) {
-		if (participant != null)
-			participants.put(participant.getId(), participant);
-	}
+  private RoomHandler roomHandler;
 
-	public void leave(Participant user) throws IOException {
+  private volatile boolean closed = false;
 
-		log.debug("PARTICIPANT {}: Leaving room {}", user.getName(), this.name);
-		this.removeParticipant(user);
-		
-		if (user.equals(screensharer)) {
-			this.screensharer = null;
-		}
-		
-		user.close();
-		
-	}
-	
-	public void leave(String userId) throws IOException {
-		Participant user = participants.get(userId);
-		
-		if (user != null)
-			leave(user);
-	}
+  private AtomicInteger activePublishers = new AtomicInteger(0);
 
-	public void joinRoom(Participant newParticipant) {
-		final JsonObject newParticipantMsg = new JsonObject();
-		newParticipantMsg.addProperty("id", "newParticipantArrived");
-		newParticipantMsg.addProperty("name", newParticipant.getName());
-		newParticipantMsg.addProperty("userId", newParticipant.getId());
-		broadcast(newParticipantMsg, newParticipant);
-	}
-	
-	private void broadcast(JsonObject message, Participant exception) {
+  private Object pipelineCreateLock = new Object();
+  private Object pipelineReleaseLock = new Object();
+  private volatile boolean pipelineReleased = false;
+  private boolean destroyKurentoClient;
 
-		for (final Participant participant : participants.values()) {
+  public Room(String roomName, KurentoClient kurentoClient, RoomHandler roomHandler,
+      boolean destroyKurentoClient) {
+    this.name = roomName;
+    this.kurentoClient = kurentoClient;
+    this.destroyKurentoClient = destroyKurentoClient;
+    this.roomHandler = roomHandler;
+    log.debug("New ROOM instance, named '{}'", roomName);
+  }
 
-			if (participant.equals(exception) || !(participant instanceof WebUser))
-				continue;
+  public String getName() {
+    return name;
+  }
 
-			try {
-				participant.sendMessage(message);
-			} catch (final IOException e) {
-				log.debug("ROOM {}: participant {} could not be notified",
-						name, participant.getName(), e);
-			}
+  public MediaPipeline getPipeline() {
+    try {
+      pipelineLatch.await(Room.ASYNC_LATCH_TIMEOUT, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    return this.pipeline;
+  }
 
-		}
-	}
-	
-	public void broadcast(JsonObject message) {
-		broadcast(message, null);
-	}
+  public void join(String participantId, String userName, boolean webParticipant)
+      throws RoomException {
 
-	private void removeParticipant(Participant participant) throws IOException {
-		participants.remove(participant.getId());
+    checkClosed();
 
-		boolean isScreensharer = (screensharer != null && participant.equals(screensharer));
+    if (userName == null || userName.isEmpty()) {
+      throw new RoomException(Code.GENERIC_ERROR_CODE, "Empty user name is not allowed");
+    }
+    for (Participant p : participants.values()) {
+      if (p.getName().equals(userName)) {
+        throw new RoomException(Code.EXISTING_USER_IN_ROOM_ERROR_CODE, "User '" + userName
+            + "' already exists in room '" + name + "'");
+      }
+    }
 
-		log.debug("ROOM {}: notifying all users that {} is leaving the room",
-				this.name, participant.getName());
-	
-		final JsonObject participantLeftJson = new JsonObject();
-		participantLeftJson.addProperty("id", "participantLeft");
-		participantLeftJson.addProperty("userId", participant.getId());
-		participantLeftJson.addProperty("name", participant.getName());
-		participantLeftJson.addProperty("isScreensharer", isScreensharer);
-		
-		final JsonArray participantsArray = new JsonArray();
-		
-		for (final Participant p : this.getParticipants()) {
-			final JsonElement participantName = new JsonPrimitive(p.getName());
-			participantsArray.add(participantName);
-		}
-		participantLeftJson.add("data", participantsArray);
-		
-		for (final Participant p : participants.values()) {
-			if (p instanceof WebUser) {
-				if (isScreensharer)
-					((WebUser) participant).cancelPresentation();
-				
-				p.sendMessage(participantLeftJson);
-			}
-		}
-		
-	}
+    createPipeline();
 
-	public void cancelPresentation() throws IOException {
-		if (screensharer != null) {
-			final JsonObject cancelPresentationMsg = new JsonObject();
-			cancelPresentationMsg.addProperty("id", "cancelPresentation");
-			cancelPresentationMsg.addProperty("userId", screensharer.getId());
-			
-			for (final Participant participant : participants.values()) {
-				if (participant instanceof WebUser) {
-					final WebUser webParticipant = (WebUser) participant;
-					webParticipant.cancelPresentation();
-					webParticipant.sendMessage(cancelPresentationMsg);
-				}
-			}
-			
-			screensharer = null;
-		}
-	}
+    participants.put(participantId, new Participant(participantId, userName, this, getPipeline(),
+        webParticipant));
 
-	public void sendInformation(Participant user, String id) throws IOException {
+    log.info("ROOM {}: Added participant {}", name, userName);
+  }
 
-		final JsonArray participantsArray = new JsonArray();
-		
-		for (final Participant participant : this.getParticipants()) {
-			if (!participant.equals(user)) {
-				final JsonElement participantName = new JsonPrimitive(
-						participant.getName());
-				participantsArray.add(participantName);
-			}
-		}
+  public void newPublisher(Participant participant) {
+    registerPublisher();
 
-		final JsonObject message = new JsonObject();
-		message.addProperty("id", id);
-		message.add("data", participantsArray);
-		message.addProperty("existingScreensharer", (screensharer != null));
-		
-		if (line != null)
-			message.addProperty("lineExtension", line.getExtension());
-		
-		if (screensharer != null) {
-			message.addProperty("presenterId", screensharer.getId());
-			message.addProperty("screensharer", screensharer.getName());
-		}
-		
-		log.debug("PARTICIPANT {}: sending a list of {} participants",
-				user.getName(), participantsArray.size());
-		
-		user.sendMessage(message);
-	}
+    // pre-load endpoints to recv video from the new publisher
+    for (Participant participant1 : participants.values()) {
+      if (participant.equals(participant1)) {
+        continue;
+      }
+      participant1.getNewOrExistingSubscriber(participant.getName());
+    }
 
-	/**
-	 * @return a collection with all the participants in the room
-	 */
-	public Collection<Participant> getParticipants() {
-		return participants.values();
-	}
+    log.debug("ROOM {}: Virtually subscribed other participants {} to new publisher {}", name,
+        participants.values(), participant.getName());
+  }
 
-	public Participant getParticipant(String id) {
-		return participants.get(id);
-	}
+  public void cancelPublisher(Participant participant) {
+    deregisterPublisher();
 
-	@Override
-	public void close() {
-		for (final Participant user : participants.values()) {
-			try {
-				user.close();
-			} catch (IOException e) {
-				log.debug("ROOM {}: Could not invoke close on participant {}",
-						this.name, user.getName(), e);
-			}
-		}
+    // cancel recv video from this publisher
+    for (Participant subscriber : participants.values()) {
+      if (participant.equals(subscriber)) {
+        continue;
+      }
+      subscriber.cancelReceivingMedia(participant.getName());
+    }
 
-		participants.clear();
+    log.debug("ROOM {}: Unsubscribed other participants {} from the publisher {}", name,
+        participants.values(), participant.getName());
 
-		compositePipeline.release(new Continuation<Void>() {
+  }
 
-			@Override
-			public void onSuccess(Void result) throws Exception {
-				log.trace("ROOM {}: Released Composite Pipeline", Room.this.name);
-			}
+  public void leave(String participantId) throws RoomException {
 
-			@Override
-			public void onError(Throwable cause) throws Exception {
-				log.warn("PARTICIPANT {}: Could not release Composite Pipeline",
-						Room.this.name);
-			}
-		});
-		
-		presentationPipeline.release(new Continuation<Void>() {
+    checkClosed();
 
-			@Override
-			public void onSuccess(Void result) throws Exception {
-				log.trace("ROOM {}: Released Presentation Pipeline", Room.this.name);
-			}
+    Participant participant = participants.get(participantId);
+    if (participant == null) {
+      throw new RoomException(Code.USER_NOT_FOUND_ERROR_CODE, "User #" + participantId
+          + " not found in room '" + name + "'");
+    }
+    log.info("PARTICIPANT {}: Leaving room {}", participant.getName(), this.name);
+    if (participant.isStreaming()) {
+      this.deregisterPublisher();
+    }
+    this.removeParticipant(participant);
+    participant.close();
+  }
 
-			@Override
-			public void onError(Throwable cause) throws Exception {
-				log.warn("PARTICIPANT {}: Could not release Presentation Pipeline",
-						Room.this.name);
-			}
-		});
-		
-		log.debug("Room {} closed", this.name);
-	}
-	
-	public MediaPipeline getCompositePipeline() {
-		return compositePipeline;
-	}
-	
-	public MediaPipeline getPresentationPipeline() {
-		return presentationPipeline;
-	}
-	
-	public void setScreensharer(WebUser user) {
-		this.screensharer = user;
-	}
+  public Collection<Participant> getParticipants() {
 
-	public boolean hasScreensharer() {
-		return (screensharer != null);
-	}
+    checkClosed();
 
-	public long setCSeq(long cseq) {
-		this.cseq = cseq;
-		return cseq;
-	}
+    return participants.values();
+  }
 
-	public long getCSeq() {
-		return this.cseq;
-	}
+  public Set<String> getParticipantIds() {
 
-	public Line getLine() {
-		return this.line;
-	}
-	
-	public String getCallId() {
-		return this.callId;
-	}
+    checkClosed();
 
-	public void setLine(Line line) {
-		this.line = line;
-	}
+    return participants.keySet();
+  }
 
-	public boolean isClosing() {
-		return closing;
-	}
+  public Participant getParticipant(String participantId) {
 
-	public void setClosing() {
-		this.closing = true;
-	}
-	
-	public int size() {
-		return participants.size();
-	}
+    checkClosed();
 
+    return participants.get(participantId);
+  }
+
+  public Participant getParticipantByName(String userName) {
+
+    checkClosed();
+
+    for (Participant p : participants.values()) {
+      if (p.getName().equals(userName)) {
+        return p;
+      }
+    }
+
+    return null;
+  }
+
+  public void close() {
+    if (!closed) {
+
+      for (Participant user : participants.values()) {
+        user.close();
+      }
+
+      participants.clear();
+
+      closePipeline();
+
+      log.debug("Room {} closed", this.name);
+
+      if (destroyKurentoClient) {
+        kurentoClient.destroy();
+      }
+
+      this.closed = true;
+    } else {
+      log.warn("Closing an already closed room '{}'", this.name);
+    }
+  }
+
+  public void sendIceCandidate(String participantId, String endpointName, IceCandidate candidate) {
+    this.roomHandler.onIceCandidate(name, participantId, endpointName, candidate);
+  }
+
+  public void sendMediaError(String participantId, String description) {
+    this.roomHandler.onMediaElementError(name, participantId, description);
+  }
+
+  public boolean isClosed() {
+    return closed;
+  }
+
+  private void checkClosed() {
+    if (closed) {
+      throw new RoomException(Code.ROOM_CLOSED_ERROR_CODE, "The room '" + name + "' is closed");
+    }
+  }
+
+  private void removeParticipant(Participant participant) {
+
+    checkClosed();
+
+    participants.remove(participant.getId());
+
+    log.debug("ROOM {}: Cancel receiving media from user '{}' for other users", this.name,
+        participant.getName());
+    for (Participant other : participants.values()) {
+      other.cancelReceivingMedia(participant.getName());
+    }
+  }
+
+  public int getActivePublishers() {
+    return activePublishers.get();
+  }
+
+  public void registerPublisher() {
+    this.activePublishers.incrementAndGet();
+  }
+
+  public void deregisterPublisher() {
+    this.activePublishers.decrementAndGet();
+  }
+
+  private void createPipeline() {
+    synchronized (pipelineCreateLock) {
+      if (pipeline != null) {
+        return;
+      }
+      log.info("ROOM {}: Creating MediaPipeline", name);
+      try {
+        kurentoClient.createMediaPipeline(new Continuation<MediaPipeline>() {
+          @Override
+          public void onSuccess(MediaPipeline result) throws Exception {
+            pipeline = result;
+            pipelineLatch.countDown();
+            log.debug("ROOM {}: Created MediaPipeline", name);
+          }
+
+          @Override
+          public void onError(Throwable cause) throws Exception {
+            pipelineLatch.countDown();
+            log.error("ROOM {}: Failed to create MediaPipeline", name, cause);
+          }
+        });
+      } catch (Exception e) {
+        log.error("Unable to create media pipeline for room '{}'", name, e);
+        pipelineLatch.countDown();
+      }
+      if (getPipeline() == null) {
+        throw new RoomException(Code.ROOM_CANNOT_BE_CREATED_ERROR_CODE,
+            "Unable to create media pipeline for room '" + name + "'");
+      }
+
+      pipeline.addErrorListener(new EventListener<ErrorEvent>() {
+        @Override
+        public void onEvent(ErrorEvent event) {
+          String desc = event.getType() + ": " + event.getDescription() + "(errCode="
+              + event.getErrorCode() + ")";
+          log.warn("ROOM {}: Pipeline error encountered: {}", name, desc);
+          roomHandler.onPipelineError(name, getParticipantIds(), desc);
+        }
+      });
+    }
+  }
+
+  private void closePipeline() {
+    synchronized (pipelineReleaseLock) {
+      if (pipeline == null || pipelineReleased) {
+        return;
+      }
+      getPipeline().release(new Continuation<Void>() {
+
+        @Override
+        public void onSuccess(Void result) throws Exception {
+          log.debug("ROOM {}: Released Pipeline", Room.this.name);
+          pipelineReleased = true;
+        }
+
+        @Override
+        public void onError(Throwable cause) throws Exception {
+          log.warn("ROOM {}: Could not successfully release Pipeline", Room.this.name, cause);
+          pipelineReleased = true;
+        }
+      });
+    }
+  }
 }
