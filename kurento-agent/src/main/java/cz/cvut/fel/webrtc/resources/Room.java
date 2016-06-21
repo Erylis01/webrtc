c5c26e1ee7bab2fae98e55b45a3266ad053701d9
Pierre <pierre.coquerel@mines-ales.org>
2016-06-21 13:27:35 +0200

    modif record

--- kurento-agent/src/main/java/cz/cvut/fel/webrtc/resources/Room.java
+++ kurento-agent/src/main/java/cz/cvut/fel/webrtc/resources/Room.java
@@ -78,8 +78,8 @@ public class Room implements Closeable {
 
 		this.compositePipeline = kurento.createMediaPipeline();
 		this.presentationPipeline = kurento.createMediaPipeline();
-		//this.composite = new Composite.Builder(compositePipeline).build();
-		this.composite = new Composite.Builder(getCompositePipeline()).build();
+		this.composite = new Composite.Builder(compositePipeline).build();
+		//this.composite = new Composite.Builder(getCompositePipeline()).build();
 
 		log.info("ROOM {} has been created", roomName);
 	}
