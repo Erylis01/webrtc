3ecd95e0b2a9cb35a82f58ff9ec87e151ccc49bc
Pierre <pierre.coquerel@mines-ales.org>
2016-06-21 09:57:14 +0200

    Fin de correction d'erreurs

--- kurento-agent/src/main/java/cz/cvut/fel/webrtc/db/LineRegistry.java
+++ kurento-agent/src/main/java/cz/cvut/fel/webrtc/db/LineRegistry.java
@@ -15,7 +15,7 @@ import org.apache.http.auth.MalformedChallengeException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-
+import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
