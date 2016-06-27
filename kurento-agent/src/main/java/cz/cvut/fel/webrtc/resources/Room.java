a3736bba16ba03f987e07542a1e08904dc66c51f
Pierre <pierre.coquerel@mines-ales.org>
2016-06-27 11:46:43 +0200

    modif du dockercompose.yml pour partager le dossier record

--- kurento-agent/src/main/java/cz/cvut/fel/webrtc/resources/Room.java
+++ kurento-agent/src/main/java/cz/cvut/fel/webrtc/resources/Room.java
@@ -103,13 +103,6 @@ public class Room implements Closeable {
 			add(participant);
 			sendInformation(participant, "compositeInfo");
 			this.hubPort = new HubPort.Builder(this.composite).build();
-			// Record
-			/*log.info("Start Recording");
-			this.hubPort = new HubPort.Builder(this.composite).build();
-			this.recorderEndpoint = new RecorderEndpoint.Builder(getCompositePipeline(),
-					"file:///record/" + getName() + ".mp4").withMediaProfile(MediaProfileSpecType.MP4).build();
-			this.hubPort.connect(this.recorderEndpoint);
-			this.recorderEndpoint.record();*/
 
 		} catch (Exception e) {
 			log.info("ROOM {}: adding participant {} failed: {}", name, userId, e);
