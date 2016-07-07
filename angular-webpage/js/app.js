5096f6a3f5ba144bab6c9a3bfc3e120ebb33bbee
AnonCaptcha <william.teysso@gmail.com>
2016-07-07 14:42:53 +0200

    *Add button to translate

--- angular-webpage/js/app.js
+++ angular-webpage/js/app.js
@@ -20,20 +20,29 @@ app.config(['$routeProvider', function($routeProvider) {
 app.config(['$translateProvider',function($translateProvider) {
         $translateProvider.translations('en', {
             'WARNING_BROWSER':'Your browser may not support WebRTC yet. Please use Mozilla Firefox, Google Chrome or Opera instead.',
-            'CONFIRMATION':'Join !'            
+            'CONFIRMATION':'Join !',
+            BUTTON_TEXT_EN: 'english',
+            BUTTON_TEXT_FR: 'français'              
         });    
         
     
         $translateProvider.translations('fr', {
             'WARNING_BROWSER':'Votre navigateur est potentiellement incompatible avec WebRTC. Veuillez utiliser Mozilla Firefox, Google Chrome ou Opera à la place.',
-            'CONFIRMATION':'Entrez !'
+            'CONFIRMATION':'Entrez !',
+            BUTTON_TEXT_EN: 'english',
+            BUTTON_TEXT_FR: 'français'
         });  
     
     
-        $translateProvider.preferredLanguage('en');
+        $translateProvider.determinePreferredLanguage();
             }]);
 
 
 // Injections
 app.controller('UserCtrl', ['$scope', '$location', 'socket', 'constraints', 'LxNotificationService', 'participants', UserCtrl]);
 app.controller('RoomCtrl', ['$scope', '$location', '$window', '$routeParams', '$timeout', 'socket', 'constraints', 'LxNotificationService', 'LxProgressService', 'participants', RoomCtrl]);
+app.controller('TranslateController', function($translate, $scope) {
+  $scope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
+  };
+});
