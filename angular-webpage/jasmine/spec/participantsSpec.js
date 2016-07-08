d0f30bff0df9e8617af78288898d55f15985d712
AnonCaptcha <william.teysso@gmail.com>
2016-07-08 14:47:27 +0200

    *Try to fix "participants" missing

--- angular-webpage/jasmine/spec/participantsSpec.js
+++ angular-webpage/jasmine/spec/participantsSpec.js
@@ -1,6 +1,6 @@
 
 
-describe("get", function() {
+describe("get", function(participants) {
         it("retrieve an user", function(){
             participants.add(14587,"Jean");
             expect(get(14587).name).toEqual("Jean");
