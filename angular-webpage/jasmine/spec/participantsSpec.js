9c49ebc2644710b7670394e9391b396695b142e6
AnonCaptcha <william.teysso@gmail.com>
2016-07-08 14:41:06 +0200

    *Try to correct the test

--- angular-webpage/jasmine/spec/participantsSpec.js
+++ angular-webpage/jasmine/spec/participantsSpec.js
@@ -2,7 +2,8 @@
 
 describe("get", function() {
         it("retrieve an user", function(){
-            pariticipants.add(14587,"Jean")
+            var participants = {};
+            pariticipants.add(14587,"Jean");
             expect(get(14587).name).toEqual("Jean");
         });       
 });
