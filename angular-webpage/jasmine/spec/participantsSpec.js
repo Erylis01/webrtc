755e9478394222592ab513c1ed802686d077c1c6
AnonCaptcha <william.teysso@gmail.com>
2016-07-08 14:44:01 +0200

    *Correct misspeling

--- angular-webpage/jasmine/spec/participantsSpec.js
+++ angular-webpage/jasmine/spec/participantsSpec.js
@@ -2,8 +2,7 @@
 
 describe("get", function() {
         it("retrieve an user", function(){
-            var participants = {};
-            pariticipants.add(14587,"Jean");
+            participants.add(14587,"Jean");
             expect(get(14587).name).toEqual("Jean");
         });       
 });
