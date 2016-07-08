381d29c10282cf1993bc0ac1caa15ea445839a54
AnonCaptcha <william.teysso@gmail.com>
2016-07-08 15:06:37 +0200

    *Try to access participants

--- angular-webpage/jasmine/spec/participantsSpec.js
+++ angular-webpage/jasmine/spec/participantsSpec.js
@@ -1,6 +1,6 @@
 
 
-describe("isEmpty", function() {
+describe("isEmpty", function(participants) {
         it("verify is the room is empty", function(participants){
             expect(participants.isEmpty()).toBe(true);
         });       
