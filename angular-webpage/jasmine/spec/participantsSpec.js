09078e7e81e0cad7d0c10d2e3dab6fca0a303fe1
AnonCaptcha <william.teysso@gmail.com>
2016-07-13 14:54:15 +0200

    *Try to add other mock ref

--- angular-webpage/jasmine/spec/participantsSpec.js
+++ angular-webpage/jasmine/spec/participantsSpec.js
@@ -1,9 +1,19 @@
-var participants;
+var mock, participants;
+
 beforeEach(angular.module('app',['ngRoute', 'ng.deviceDetector', 'lumx','pascalprecht.translate']));
+
 beforeEach(function() {
+    
+  mock = {alert: jasmine.createSpy()};
+    
+  module(function($provide) {
+    $provide.value('socket', mock);
+  });
+    
   inject(function($injector) {
     participants = $injector.get('participants');
   });
+    
 });
 
 
