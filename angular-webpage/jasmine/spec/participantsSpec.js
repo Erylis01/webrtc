613067890b461e7c05937242a3bdc0d1bedf8ae2
AnonCaptcha <william.teysso@gmail.com>
2016-07-15 09:23:50 +0200

    *Adding more test in participants, pre-backup commit

--- angular-webpage/jasmine/spec/participantsSpec.js
+++ angular-webpage/jasmine/spec/participantsSpec.js
@@ -1,3 +1,10 @@
+/**
+* Test file for participants.js factory 
+*
+* We consider that getter is fully functionnal. The webSocket is used with angular 
+* mocks.js. This testfile could be run with grunt without setting up application.
+*/
+
 describe('Unit testing for participants factory', function() {
    
 var mock, participants;
@@ -18,12 +25,29 @@ beforeEach(function() {
     
 });
 
-
-it("verify participants definition", function() {
+it("verify participants factory injection", function() {
 expect(participants).not.toBe(undefined);
 }); 
  
-it("verify the emptyness checking", function() {
+it("verify the object participant",function() {
+    //Call the add function and check basic property
+    participants.add("145RBC","Jean");
+    expect(participants.get("145RBC").name).not.toBe(undefined); 
+    expect(participants.get("145RBC").name).toBe("Jean");
+});
+
+//Test for Remove() 
+it("verify the participant remover - remove()", function() {
+    participants.add("145RBC","Jean");
+    expect(participants.get("145RBC")).not.toBe(undefined);
+    participants.remove("145RBC");
+    expect(participants.get("145RBC")).toBe(undefined);
+});    
+    
+//Test for isEmpty()     
+it("verify the emptyness checking - isEmpty()", function() {
     expect(participants.isEmpty()).toBe(true);
-});       
+});
+    
+    
 });
