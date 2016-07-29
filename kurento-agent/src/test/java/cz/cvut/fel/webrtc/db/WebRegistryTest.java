ada6bccbc83fb08a3bee67016ff604758172bcce
Erylis01 <pierre.coquerel@mines-ales.org>
2016-07-29 13:43:05 +0200

    Génération de la javadoc

---
+++ kurento-agent/src/test/java/cz/cvut/fel/webrtc/db/WebRegistryTest.java
@@ -0,0 +1,49 @@
+package cz.cvut.fel.webrtc.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WebRegistryTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBySession() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveBySession() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+}
